390fa679d638e55b7628246147c1a32786c0685f
riccardodesanti <riccardodesantimail@gmail.com>
2018-05-16 12:30:30 +0200

    debug

--- index.js
+++ index.js
@@ -274,6 +274,6 @@ function findAndShow(lat, lng) {
    request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDFcTJgoRraYVYamm4msIbDrjt51WWDeZo', { json: true }, (err, res, body) => {
      console.log(body);
   });
-  console.log("function called");
+  console.log("the env vars are: place:"+place+", lat:"+lat+", lng: "+lng+", distance"+distance);
 
 }
