10d7eac498d5782c4ea78c7fda20d2625034f0ab
riccardodesanti <riccardodesantimail@gmail.com>
2018-05-16 12:44:14 +0200

    debug

--- index.js
+++ index.js
@@ -269,11 +269,12 @@ function askPosition(sender_psid) {
 
 // Finds the places through the Google Places API and shows them in chat
 function findAndShow(lat, lng) {
-
-  // const myKey = "AIzaSyDFcTJgoRraYVYamm4msIbDrjt51WWDeZo";
-  //  request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDFcTJgoRraYVYamm4msIbDrjt51WWDeZo', { json: true }, (err, res, body) => {
-  //    console.log(body);
-  // });
+  if ( distance == undefined ) { distance = 30; }
   console.log("the env vars are: place:"+place+", lat:"+lat+", lng: "+lng+", distance"+distance);
+
+  const myKey = "AIzaSyDFcTJgoRraYVYamm4msIbDrjt51WWDeZo";
+   request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + lng + '&radius=' + distance*1000 + '1500&type=' + place + '&key=AIzaSyDFcTJgoRraYVYamm4msIbDrjt51WWDeZo', { json: true }, (err, res, body) => {
+     console.log(body);
+  });
 
 }
